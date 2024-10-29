4bb8550f32d385a9e2b64cffe9fa3e86b7d78306
Nihadname <nihadcoding@gmail.com>
2024-10-30 02:39:07 +0400

    done

--- 03-modules.js
+++ 03-modules.js
@@ -1,7 +1,8 @@
 var names =require('./04-names');
 var SayHi=require('./05-utils');
+var data=require('./06-alternative-flavor');
 console.log(names)
-
+console.log(data);
  SayHi(names.john);
  SayHi(names.peter)
 SayHi("susan")
